021853dc4a5c305c51cf27ea0c5ce6f302dc97c8
duyanming <dym880@163.com>
2020-10-12 09:39:58 +0800

    独立监控插件

---
+++ Anno.Plugs.MonitorService/Bootstrap.cs
@@ -0,0 +1,18 @@
+﻿using Anno.EngineData;
+using System;
+
+namespace Anno.Plugs.MonitorService
+{
+    public class Bootstrap : IPlugsConfigurationBootstrap
+    {
+        public void ConfigurationBootstrap()
+        {
+
+        }
+
+        public void PreConfigurationBootstrap()
+        {
+
+        }
+    }
+}
---
+++ Anno.Plugs.MonitorService/ResourceModule.cs
@@ -0,0 +1,28 @@
+﻿/****************************************************** 
+Writer:Du YanMing
+Mail:dym880@163.com
+Create Date:2020/10/12 9:19:12 
+Functional description： Resource
+******************************************************/
+using Anno.Const.Attribute;
+using Anno.EngineData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anno.Plugs.MonitorService
+{
+    public class ResourceModule : BaseModule
+    {
+        private static readonly EngineData.SysInfo.UseSysInfoWatch Usi = new EngineData.SysInfo.UseSysInfoWatch();
+        /// <summary>
+        /// 服务资源信息
+        /// </summary>
+        /// <returns></returns>
+        [AnnoInfo(Desc = "服务资源信息CPU、Memory")]
+        public ActionResult GetServerStatus()
+        {
+            return new ActionResult(true, Usi.GetServerStatus());
+        }
+    }
+}
