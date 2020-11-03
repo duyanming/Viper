6d0c609e42e349324e5a88bd71a4651138df10a5
duyanming <dym880@163.com>
2020-11-03 09:38:37 +0800

    示例说明

--- Samples/Plugs/Anno.Plugs.HelloWorldService/HelloWorldBootStrap.cs
+++ Samples/Plugs/Anno.Plugs.HelloWorldService/HelloWorldBootStrap.cs
@@ -3,13 +3,29 @@ using System;
 
 namespace Anno.Plugs.HelloWorldService
 {
+    /// <summary>
+    /// 插件启动引导器
+    /// DependsOn 依赖的类型程序集自动注入DI容器
+    /// </summary>
+    [DependsOn(
+        //typeof(Domain.Bootstrap)
+        //, typeof(QueryServices.Bootstrap)
+        //, typeof(Repository.Bootstrap)
+        //, typeof(Command.Handler.Bootstrap
+        )]
     public class HelloWorldBootStrap : IPlugsConfigurationBootstrap
     {
+        /// <summary>
+        /// Service 依赖注入构建之后调用
+        /// </summary>
         public void ConfigurationBootstrap()
         {
             //throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Service 依赖注入构建之前调用
+        /// </summary>
+        /// </summary>
         public void PreConfigurationBootstrap()
         {
             //throw new NotImplementedException();
