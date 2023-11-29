83b549b81315ee82f95fa43d0b7ef3b0d471676c
duyanming <dym6295@163.com>
2023-11-29 19:48:46 +0800

    获取客户端真实IP

--- Viper/Extensions/Middleware/AnnoWebHostBuilderExtensions.cs
+++ Viper/Extensions/Middleware/AnnoWebHostBuilderExtensions.cs
@@ -208,7 +208,11 @@ namespace Microsoft.AspNetCore
             {
                 if (headers != null && headers.ContainsKey("X-Original-For") && !headers["X-Original-For"].ToArray()[0].StartsWith("127.0.0.1"))
                 {
-                    input.Add("X-Original-For", headers["X-Original-For"].ToArray()[0]);
+                    input.Add("X-Original-For", headers["X-Original-For"].ToArray()[0].Replace("::ffff:", ""));
+                }
+                else if (headers != null && headers.ContainsKey("X-Forwarded-For") && !headers["X-Forwarded-For"].ToArray()[0].StartsWith("127.0.0.1"))
+                {
+                    input.Add("X-Original-For", headers["X-Forwarded-For"].ToArray()[0].Replace("::ffff:", ""));
                 }
                 else
                 {
